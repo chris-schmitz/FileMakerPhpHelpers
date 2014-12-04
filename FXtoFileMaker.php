ba116211775bce10696d8d1fd4986e69cf3362cf
Christopher Schmitz <schmitz.chris@gmail.com>
2014-12-04 17:21:10 -0600

    Adding a check to make sure an Error object was not passed into the function.

--- FXtoFileMaker.php
+++ FXtoFileMaker.php
@@ -4,11 +4,18 @@
 /**
  * Creates an array of data from a FileMaker Result object that mimics the FX result array structure.
  *
+ * This function assumes that the FileMaker PHP Library has already been required or `use`d.
+ *
  * @param \FileMaker\Result FileMaker PHP API Result object
  *
  * @return array Records in FX result structure form
  */
 function createMockFXRecordArray($fileMakerResultObject){
+
+    if(FileMaker::isError($fileMakerResultObject)){
+        // throw new Exception("The result set is a FileMaker Error Object");
+        return null;
+    }
 
     $fields  = $fileMakerResultObject->getFields();
     $records = [];
